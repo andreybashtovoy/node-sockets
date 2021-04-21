dea257f79b5fff44490705b6f74d8333d6f0c674
andreybashtovoy <rovage.abc@gmail.com>
2021-04-21 16:00:10 +0300

    sockets

--- websockets/server.js
+++ websockets/server.js
@@ -2,6 +2,9 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
+const WebSocket = require('ws')
+const UUID = require('uuid')
+
 const host = 'localhost';
 const port = '8000';
 
@@ -25,4 +28,37 @@ const server = http.createServer(requestListener);
 
 server.listen(port, host, () => {
     console.log(`Server is running on http://${host}:${port}`);
+});
+
+
+
+var WebSocketServer = new require('ws');
+
+// подключённые клиенты
+var clients = {};
+
+// WebSocket-сервер на порту 8081
+var webSocketServer = new WebSocketServer.Server({
+    port: 3001
+});
+
+webSocketServer.on('connection', function(ws) {
+
+    var id = Math.random();
+    clients[id] = ws;
+    console.log("новое соединение " + id);
+
+    ws.on('message', function(message) {
+        console.log('получено сообщение ' + message);
+
+        for (var key in clients) {
+            clients[key].send(message);
+        }
+    });
+
+    ws.on('close', function() {
+        console.log('соединение закрыто ' + id);
+        delete clients[id];
+    });
+
 });
--- websockets/src/js/socket.js
+++ websockets/src/js/socket.js
@@ -10,6 +10,32 @@ if (localStorage.getItem('user_id') == undefined) {
 }
 user_id = localStorage.getItem('user_id');
 
+let socket = new WebSocket("ws://localhost:3001");
+
+socket.onopen = function(e) {
+    alert("[open] Соединение установлено");
+    alert("Отправляем данные на сервер");
+    socket.send("Меня зовут Джон");
+};
+
+socket.onmessage = function(event) {
+    console.log(`[message] Данные получены с сервера: ${event.data}`);
+};
+
+socket.onclose = function(event) {
+    if (event.wasClean) {
+        console.log(`[close] Соединение закрыто чисто, код=${event.code} причина=${event.reason}`);
+    } else {
+        // например, сервер убил процесс или сеть недоступна
+        // обычно в этом случае event.code 1006
+        console.log('[close] Соединение прервано');
+    }
+};
+
+socket.onerror = function(error) {
+    console.log(`[error] ${error.message}`);
+};
+
 // socket.on('all mess', function(data, users) {
 //     $('.chat').empty();
 //     for (let i in data) {
